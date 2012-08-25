88064f3c7af35ea591838a102c8f999b50c35742
Russ Garrett <russ@garrett.co.uk>
2012-08-25 21:08:07 +0100

    Initial lights library

--- lights.h
+++ lights.h
@@ -1,12 +1,18 @@
-#include "Arduino.h"
-
 #ifndef LIGHTS_H
 #define LIGHTS_H
+
+#include "Arduino.h"
+
+enum LED_Pins { LED_RIGHT = 7, LED_LEFT = 4, LED_BLUE = 5, LED_GREEN = 6, LED_RED = 10 };
+
+struct Colour {
+  char red, green, blue;
+};
 
 class Lights {
   public:
   Lights();
-       
+  void set(int light, Colour colour);
 };
 
 #endif // LIGHTS_H
