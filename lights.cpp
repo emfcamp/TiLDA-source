c127045ddd7167d1e67dea17af58d07e6e4583cf
Russ Garrett <russ@garrett.co.uk>
2012-08-26 02:08:28 +0100

    Rejig

--- lights.cpp
+++ lights.cpp
@@ -25,3 +25,18 @@ void Lights::set(int light, Colour colour) {
     digitalWrite(LED_RIGHT, LOW);
   }
 }
+
+void led_cycle(Lights *lights, int light, int time) {
+  Colour c;
+  char brightness = 128;
+  float step = (2 * PI) / time;
+  for (float i = 0; i < 2 * PI; i += step) {
+    c.red = (int)(abs(sin(i)) * brightness);
+    c.green = (int)(abs(sin(i + PI / 2)) * brightness);
+    c.blue = (int)(abs(sin(i + PI)) * brightness);
+    lights->set(light, c);
+    delay(1);
+  }
+  c.red = 0; c.green = 0; c.blue = 0;
+  lights->set(light, c);
+}
