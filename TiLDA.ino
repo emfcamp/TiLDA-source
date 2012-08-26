945b551eb36c5705e5ffddb91d1cd19456850a9b
Russ Garrett <russ@garrett.co.uk>
2012-08-26 03:28:20 +0100

    Make the bootup sequence even faster

--- TiLDA.ino
+++ TiLDA.ino
@@ -30,8 +30,8 @@ void button_press() {
 }
 
 void setup() {
-  led_cycle(&lights, PIN_LED_LEFT, 500);
-  led_cycle(&lights, PIN_LED_RIGHT, 500);
+  led_cycle(&lights, PIN_LED_LEFT, 250);
+  led_cycle(&lights, PIN_LED_RIGHT, 250);
   pinMode(PIN_BUTTON, INPUT_PULLUP);
   attachInterrupt(INT_BUTTON, button_press, CHANGE);
 }
