fbc3bf7c163f3d56fa536ba9f438aec927ca20bc
Russ Garrett <russ@garrett.co.uk>
2012-08-26 02:21:01 +0100

    Add a separate header file for naming our custom pins

--- TiLDA.ino
+++ TiLDA.ino
@@ -3,12 +3,13 @@
 Lights lights;
 
 void cycle_lights() {
+  // Cycle through the lights on startup
   for (float i = 0; i < 2*PI; i += 0.01) {
     analogWrite(13, (int)(abs(sin(i)) * 255));
     delay(1);
   }
-  led_cycle(&lights, LED_LEFT, 1000);
-  led_cycle(&lights, LED_RIGHT, 1000);
+  led_cycle(&lights, PIN_LED_LEFT, 1000);
+  led_cycle(&lights, PIN_LED_RIGHT, 1000);
 }
 
 void setup() {
---
+++ pins.h
@@ -0,0 +1,14 @@
+#ifndef PINS_H
+#define PINS_H
+
+enum Pins { PIN_LED_RIGHT = 7,
+            PIN_LED_LEFT = 4,
+            PIN_LED_BLUE = 5,
+            PIN_LED_GREEN = 6,
+            PIN_LED_RED = 10,
+            PIN_IR_IN = 11,
+            PIN_IR_OUT = 3,
+            PIN_BUTTON = 2
+          };
+
+#endif // PINS_H
