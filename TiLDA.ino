de5867ed77495b631fe3c03def566e51f1bb1d5b
Russ Garrett <russ@garrett.co.uk>
2012-08-26 03:23:49 +0100

    Torch mode!

--- TiLDA.ino
+++ TiLDA.ino
@@ -1,20 +1,43 @@
 #include "lights.h"
+#include "pins.h"
 
 Lights lights;
 
-void cycle_lights() {
-  // Cycle through the lights on startup
-  for (float i = 0; i < 2*PI; i += 0.01) {
-    analogWrite(13, (int)(abs(sin(i)) * 255));
-    delay(1);
+unsigned long last_button_down = 0;
+bool torch_on = false;
+
+void button_press() {
+  // Interrupt handler when button state changes
+  int state = digitalRead(PIN_BUTTON);
+  if (state == LOW) { // Low means pressed
+    last_button_down = millis();
+  } else if (state == HIGH) {
+    if (last_button_down != 0 && last_button_down < millis() - 1000) {
+      // Engage torch mode
+      Colour c;
+      c.green = c.blue = c.red = 255;
+      lights.set(PIN_LED_LEFT, c);
+      torch_on = true;
+      return;
+    }
+    if (torch_on) {
+      Colour c;
+      c.green = c.blue = c.red = 0;
+      lights.set(PIN_LED_LEFT, c);
+      torch_on = false;
+    }
   }
-  led_cycle(&lights, PIN_LED_LEFT, 1000);
-  led_cycle(&lights, PIN_LED_RIGHT, 1000);
 }
 
 void setup() {
-  cycle_lights();
+  led_cycle(&lights, PIN_LED_LEFT, 500);
+  led_cycle(&lights, PIN_LED_RIGHT, 500);
+  pinMode(PIN_BUTTON, INPUT_PULLUP);
+  attachInterrupt(INT_BUTTON, button_press, CHANGE);
 }
 
 void loop() {
+
 }
+
+
