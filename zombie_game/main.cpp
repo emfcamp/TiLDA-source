3f8457df3877fd956cc297a16cdbf668c07c5e26
Ptolom <ptolom@hexifact.co.uk>
2012-09-02 00:30:50 +0100

    Set up interrupt timer to trigger pings

--- zombie_game/main.cpp
+++ zombie_game/main.cpp
@@ -8,5 +8,8 @@
 #include <avr/interrupts.h>
 
 void main(){
+  OCR0A= PING_DELAY; //set interrupt interval
+  TCCR0B=(1<<WGM1)|(1<<CS02)|(1<<CS00); //set CTC mode and enable clock with 1/1024 scaler
+  TIMSK0=(1<<OCIE0A); //enable interrupt
   rf_setup();
-
+}
